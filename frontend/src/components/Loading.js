48d769de93e055b3992a1bdf960f39d4e7d6e4a9
Bharath Choudhary <47634969+coder-KB@users.noreply.github.com>
2021-05-11 11:09:56 +0530

    added loading page with css

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+    return (
+        <div className="Loading">
+            <h2>Generating Time Table ...</h2>
+            <div className="Kinetic"></div>
+        </div>
+    );
+};
+
+export default Loading;

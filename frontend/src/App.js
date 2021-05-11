9736c7f3bf1509a0c57ab71b537876cce8b9255f
Bharath Choudhary <47634969+coder-KB@users.noreply.github.com>
2021-05-11 11:08:06 +0530

    added button click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Timetable from "./components/Timetable";
 
 function App() {
-  return (
-    <div className="App">
-      <Timetable />
-    </div>
-  );
+    const [timetable, setTimetable] = useState(false);
+
+    return (
+        <div className="App">
+            {timetable ? (
+                <Timetable />
+            ) : (
+                <button
+                    className="Button"
+                    onClick={() => {
+                        setTimetable(true);
+                    }}
+                >
+                    Generate
+                </button>
+            )}
+        </div>
+    );
 }
 
 export default App;

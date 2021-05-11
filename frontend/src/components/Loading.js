f4586c3040d42e6ac97de86a9711d9595aa9e040
Bharath Choudhary <bhchoudh@cisco.com>
2021-05-11 13:31:05 +0530

    added text animation

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Loading.css";
 
 const Loading = () => {
+    const [idx, setIdx] = useState(0);
+
+    const text = "Generating Time Table ...";
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIdx((idx) => (idx === text.length ? 1 : idx + 1));
+        }, 100);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="Loading">
-            <h2>Generating Time Table ...</h2>
+            <h2>{text.slice(0, idx)}</h2>
             <div className="Kinetic"></div>
         </div>
     );

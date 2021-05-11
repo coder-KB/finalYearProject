5f0e1d421ad0d47fa07b76c1774535d6d0d118be
Bharath Choudhary <47634969+coder-KB@users.noreply.github.com>
2021-05-11 11:10:50 +0530

    added loading feature

--- frontend/src/components/Timetable.js
+++ frontend/src/components/Timetable.js
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { getTimeTable } from "../api/timetable";
 import SingleClass from "./SingleClass";
+import Loading from "./Loading";
 
 const Timetable = () => {
-  const [timetable, setTimetable] = useState();
+    const [timetable, setTimetable] = useState();
+    const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    getTimeTable().then((data) => {
-      setTimetable(data);
-    });
-  }, []);
+    useEffect(() => {
+        setLoading(true);
+        getTimeTable().then((data) => {
+            setTimetable(data);
+            setLoading(false);
+        });
+    }, []);
 
-  return (
-    <div>
-      {timetable &&
-        timetable.map((tt, ind) => (
-          <SingleClass key={ind} clsName={tt.className} result={tt.result} />
-        ))}
-    </div>
-  );
+    return (
+        <div>
+            {loading && <Loading />}
+            {timetable &&
+                timetable.map((tt, ind) => (
+                    <SingleClass
+                        key={ind}
+                        clsName={tt.className}
+                        result={tt.result}
+                    />
+                ))}
+        </div>
+    );
 };
 
 export default Timetable;

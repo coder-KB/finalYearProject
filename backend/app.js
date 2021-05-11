ae7e15ba3cc1054eb86c8bf6da2db11ea6b40c21
Bharath Choudhary <47634969+coder-KB@users.noreply.github.com>
2021-05-11 11:11:33 +0530

    changed console logs

--- backend/app.js
+++ backend/app.js
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
 app.use("/timetable", timetableRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`backend listening at http://localhost:${port}`);
 });

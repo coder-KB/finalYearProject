3e662e5f2bc42a4a330b20d5187078bfc0b5e650
Bharath Choudhary <47634969+coder-KB@users.noreply.github.com>
2021-05-13 10:27:46 +0530

    added teacher routes

---
+++ backend/routers/teacher.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var router = express.Router();
+
+const {
+  test,
+  createTeacher,
+  getTeachersList,
+  getTeacherByInitial,
+  getSingleTeacher,
+  updateTeacher,
+  removeTeacher,
+} = require("../controllers/teacher");
+
+router.get("/", test); // remove this later
+
+router.param("initial", getTeacherByInitial);
+
+router.post("/create", createTeacher);
+router.get("/all", getTeachersList);
+router.get("/:initial", getSingleTeacher);
+router.put("/:initial", updateTeacher);
+router.delete("/:initial", removeTeacher);
+
+module.exports = router;

9145c7e467955ae9b86d57bdcbe7d3da8208329f
Bharath Choudhary <47634969+coder-KB@users.noreply.github.com>
2021-05-13 10:31:43 +0530

    added mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const timetableRoutes = require("./routers/timetable");
+const teacherRoutes = require("./routers/teacher");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 const port = 4000;
 
 app.use(express.json());
-app.use(cors({origin: "*"}));
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
   res.send("Hello There");
 });
 
 app.use("/timetable", timetableRoutes);
+app.use("/teacher", teacherRoutes);
 
 app.listen(port, () => {
+  mongoose
+    .connect("mongodb://localhost:27017/fyp", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((response) => {
+      console.log("DB Connected");
+    });
   console.log(`backend listening at http://localhost:${port}`);
 });

5a8eaeb976b46526fc19b0172865bf4aa2909aab
Bharath Choudhary <bhchoudh@cisco.com>
2021-05-13 10:58:05 +0530

    added indentation

--- backend/app.js
+++ backend/app.js
@@ -10,20 +10,20 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
-  res.send("Hello There");
+    res.send("Hello There");
 });
 
 app.use("/timetable", timetableRoutes);
 app.use("/teacher", teacherRoutes);
 
 app.listen(port, () => {
-  mongoose
-    .connect("mongodb://localhost:27017/fyp", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((response) => {
-      console.log("DB Connected");
-    });
-  console.log(`backend listening at http://localhost:${port}`);
+    mongoose
+        .connect("mongodb://localhost:27017/fyp", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then((response) => {
+            console.log("DB Connected");
+        });
+    console.log(`backend listening at http://localhost:${port}`);
 });
